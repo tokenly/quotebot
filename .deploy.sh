702c56a157799b63fef98c769c9e40750a75ec5b
Devon Weller <dweller@devonweller.com>
2015-02-12 13:08:55 -0600

    added deploy script

---
+++ .deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo; echo "updating dependencies";
+/usr/local/bin/composer.phar install --prefer-dist
+
+# echo; echo "updating bower dependencies";
+# $(cd public && bower -q install)
