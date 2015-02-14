ababb7213b488c597924c15a18ee4a0c56f2c625
Devon Weller <dweller@devonweller.com>
2015-02-14 12:29:50 -0600

    lock laravel dependencies and update deploy

--- .deploy.sh
+++ .deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -e
+
 echo; echo "updating dependencies";
-/usr/local/bin/composer.phar install --prefer-dist
+/usr/local/bin/composer.phar install --prefer-dist --no-progress
 
 echo; echo "updating bower dependencies";
 $(cd public && bower -q install)
