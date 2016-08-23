019c39d926c11c10a2aa620bdd69182e286fb780
Devon Weller <devon@tokenly.co>
2016-08-23 11:33:59 -0500

    Removed GEMZ and SWARM since they were delisted

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,7 +32,9 @@ class Kernel extends ConsoleKernel {
         $load_cron = '* * * * *';
         $schedule->command('quotebot:load bitcoinAverage USD:BTC')->cron($load_cron);
         $schedule->command('quotebot:load bitstamp       USD:BTC')->cron($load_cron);
-        $schedule->command('quotebot:load poloniex       BTC:LTBC BTC:FLDC BTC:GEMZ BTC:SWARM BTC:SJCX BTC:XCP BTC:BCY')->cron($load_cron);
+        // $schedule->command('quotebot:load poloniex       BTC:LTBC BTC:FLDC BTC:GEMZ BTC:SWARM BTC:SJCX BTC:XCP BTC:BCY')->cron($load_cron);
+        // $schedule->command('quotebot:load poloniex       BTC:LTBC BTC:FLDC BTC:SWARM BTC:SJCX BTC:XCP BTC:BCY')->cron($load_cron);
+        $schedule->command('quotebot:load poloniex       BTC:LTBC BTC:FLDC BTC:SJCX BTC:XCP BTC:BCY')->cron($load_cron);
 
         // 1 minute after the hour
         $aggregate_cron = '1 * * * *';
@@ -40,8 +42,8 @@ class Kernel extends ConsoleKernel {
         $schedule->command('quotebot:aggregate-quotes bitstamp       USD BTC'  )->cron($aggregate_cron);
         $schedule->command('quotebot:aggregate-quotes poloniex       BTC LTBC' )->cron($aggregate_cron);
         $schedule->command('quotebot:aggregate-quotes poloniex       BTC FLDC' )->cron($aggregate_cron);
-        $schedule->command('quotebot:aggregate-quotes poloniex       BTC GEMZ' )->cron($aggregate_cron);
-        $schedule->command('quotebot:aggregate-quotes poloniex       BTC SWARM')->cron($aggregate_cron);
+        // $schedule->command('quotebot:aggregate-quotes poloniex       BTC GEMZ' )->cron($aggregate_cron);
+        // $schedule->command('quotebot:aggregate-quotes poloniex       BTC SWARM')->cron($aggregate_cron);
         $schedule->command('quotebot:aggregate-quotes poloniex       BTC SJCX' )->cron($aggregate_cron);
         $schedule->command('quotebot:aggregate-quotes poloniex       BTC XCP'  )->cron($aggregate_cron);
         $schedule->command('quotebot:aggregate-quotes poloniex       BTC BCY'  )->cron($aggregate_cron);
